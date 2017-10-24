7712f9b2c1a9bed38e5a7ebbf6f6c5456be2c90f
Patrick Sigourney <psigourney@gmail.com>
2017-10-24 17:24:09 -0500

    Find Nearby working okay

--- app/src/main/java/com/example/apt_miniproject_android/FindNearbyActivityAbstract.java
+++ app/src/main/java/com/example/apt_miniproject_android/FindNearbyActivityAbstract.java
@@ -1,13 +1,22 @@
 package com.example.apt_miniproject_android;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.VolleyError;
@@ -15,6 +24,7 @@ import com.example.apt_miniproject_android.backend.DefaultServerErrorAction;
 import com.example.apt_miniproject_android.backend.ServerCommunicator;
 import com.example.apt_miniproject_android.backend.ServerErrorAction;
 import com.example.apt_miniproject_android.backend.ServerResponseAction;
+import com.example.apt_miniproject_android.model.StreamInfo;
 import com.example.apt_miniproject_android.model.StreamItemInfo;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -27,78 +37,40 @@ import com.google.gson.GsonBuilder;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindNearbyActivityAbstract extends AbstractLocationActivity {
+
+    private GridView gridview;
+    private FindNearbyActivityAbstract.ImageURLAdapter adapter;
+
+
+    public void viewAllStreamsOnClick(View view){
+        Intent i = new Intent(view.getContext(), ViewStreamsActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_nearby);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        // set gridview adapter and click behavior
+        gridview = (GridView) findViewById(R.id.gridview);
+        adapter = new FindNearbyActivityAbstract.ImageURLAdapter(this);
+        gridview.setAdapter(adapter);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        Log.d(TAG, "onRequestPermissionsResult");
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //Nothing to do here?
-                } else {
-                    Log.d(TAG, "ACCESS_FINE_LOCATION permissions denied by user");
-                }
-                return;
-            }
-        }
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Log.d(TAG, "onConnected(bundle)");
-        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if (location == null) {
-                if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-                    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-            } else {
-                Log.d(TAG, "location is not null");
-                handleNewLocation(location);
-            }
-        }
-        else{
-            Log.d(TAG, "onConnected(bundle), checkSelfPermission failed");
-            return;
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d(TAG, "onPause");
-        super.onPause();
-
-        if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            mGoogleApiClient.disconnect();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        Log.d(TAG, "onResume");
-        super.onResume();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d(TAG, "onLocationChanged");
-        handleNewLocation(location);
-    }
-
-    private void handleNewLocation(Location location) {
+    protected void handleNewLocation(Location location) {
         Log.d(TAG, "handleNewLocation: " + location.toString());
 
         View view = findViewById(android.R.id.content);
 
-        Location currentLocation = new Location("");
+        final Location currentLocation = new Location("");
         currentLocation.setLatitude(location.getLatitude());
         currentLocation.setLongitude(location.getLongitude());
 
@@ -117,59 +89,147 @@ public class FindNearbyActivityAbstract extends AbstractLocationActivity {
             }
         };
 
+
         ServerResponseAction allStreamItemsCallback = new ServerResponseAction() {
             @Override
             public void handleResponse(String response) {
                 Gson gson = new GsonBuilder().create();
                 StreamItemInfo[] items = gson.fromJson(response, StreamItemInfo[].class);
 
-                //Location currentLocation
-                for(StreamItemInfo item : items){
-                    //Location loc2 = new Location("");
-                    //loc2.setLatitude(lat2);
-                    //loc2.setLongitude(lon2);
-
-                    //float distanceInMeters = loc1.distanceTo(loc2);
-
-
+                ArrayList<StreamItemInfo> itemsWithLocation = new ArrayList<>();
+                for(StreamItemInfo item : items) {
+                    if (item.getLat() != "0" && item.getLng() != "0"){
+                        itemsWithLocation.add(item);
+                    }
                 }
 
+                for(StreamItemInfo item : itemsWithLocation){
+                    Log.d("Find distance item: ", item.getId() + " lat:" + item.getLat() + " lng:" + item.getLng());
+                    Location itemLoc = new Location("");
+                    itemLoc.setLatitude((long)Double.parseDouble(item.getLat()));
+                    itemLoc.setLongitude((long)Double.parseDouble(item.getLng()));
+                    float distanceInMeters = currentLocation.distanceTo(itemLoc);
+                    String distance ="";
+                    if(distanceInMeters < 10000) {
+                        distance = Double.toString(Math.round(distanceInMeters*1000.0)/1000.0) + " M";
+                    }
+                    else{
+                        double distanceInKilometers = distanceInMeters / 1000.0000;
+                        distance = Double.toString(Math.round(distanceInKilometers*1000.0)/1000.0) + " KM";
+                    }
+                    Log.d("Distance: ", distance);
 
+                    adapter.addThumbURL(new FindNearbyActivityAbstract.ImageURL(item.getImageUrl(), distance));
+                }
 
+                adapter.notifyDataSetChanged();
             }
         };
 
-        //Get a list of all Stream Items
         comm.setErrorAction(errorAction).requestAllStreamItemInfoData(allStreamItemsCallback);
-
-
-
-
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
-        Log.i(TAG, "Location services suspended. Please reconnect.");
+    protected void onStart() {
+        super.onStart();
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG, "onConnectionFailed");
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
-            } catch (IntentSender.SendIntentException e) {
-                e.printStackTrace();
-            }
-        } else {
-            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+    public class ImageURL{
+        public String url;
+        public String name;
+
+        public ImageURL(String url, String name) {
+            this.url = url;
+            this.name = name;
         }
     }
 
-    public void viewAllStreamsOnClick(View view){
+    public class ImageURLAdapter extends BaseAdapter {
+        private Context mContext;
+        private List<FindNearbyActivityAbstract.ImageURL> mThumbURLs;
+        private int width;
+        private GridView.LayoutParams parms;
+
+        public ImageURLAdapter(Context c) {
+            mContext = c;
+            mThumbURLs = new ArrayList<>();
+        }
+
+        private void setWidth(int parent_width) {
+            int spacing = parent_width / 25;
+            width = (parent_width - 3 * spacing) / 4;
+
+            parms = new GridView.LayoutParams(width, 100 + width);
+            gridview.setHorizontalSpacing(spacing);
+            gridview.setVerticalSpacing(spacing);
+        }
 
 
+        public int getCount() {
+            return mThumbURLs.size();
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            FindNearbyActivityAbstract.ImageURLAdapter.myImageView imageView;
+
+            if (width != parent.getMeasuredWidth())
+                setWidth(parent.getMeasuredWidth());
+
+            imageView = new FindNearbyActivityAbstract.ImageURLAdapter.myImageView(mContext);
+            imageView.setLayoutParams(parms);
+
+            if(!mThumbURLs.get(position).name.equals(""))
+                imageView.setImageURL(mThumbURLs.get(position), width);
+
+            return imageView;
+        }
+
+        public void addThumbURL(FindNearbyActivityAbstract.ImageURL url){
+            mThumbURLs.add(url);
+        }
+
+        private class myImageView extends LinearLayout {
+
+            private ImageView imageView;
+            private TextView textView;
+
+            public myImageView(Context context) {
+                super(context);
+                this.setOrientation(LinearLayout.VERTICAL);
+            }
+
+            public void setImageURL(final FindNearbyActivityAbstract.ImageURL url, int width){
+
+                imageView = new ImageView(mContext);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setLayoutParams(new GridView.LayoutParams(width, width));
+
+                textView = new TextView(mContext);
+
+                if(!url.url.equals(""))
+                    Picasso.with(mContext)
+                            .load(url.url)
+                            .placeholder(android.R.drawable.picture_frame)
+                            .into(imageView);
+
+                textView.setText(url.name);
+
+                this.addView(imageView);
+                this.addView(textView);
+            }
+
+
+        }
 
     }
+
 }
--- app/src/main/java/com/example/apt_miniproject_android/model/StreamItemInfo.java
+++ app/src/main/java/com/example/apt_miniproject_android/model/StreamItemInfo.java
@@ -1,34 +1,39 @@
 package com.example.apt_miniproject_android.model;
 
 public class StreamItemInfo {
-    private long streamId;
-    private String streamName;
-    private String imageUrl;
-    private String dateAdded;
+    private String dateadded;
+    private String imageurl;
+    private String streamname;
+    private long streamid;
     private String lat;
     private String lng;
 
     public long getId(){
-        return streamId;
+        return streamid;
     }
 
     public String getStreamName(){
-        return streamName;
+        if(streamname == null || streamname.trim() == "") return "0";
+        else return streamname.trim();
     }
 
     public String getImageUrl(){
-        return imageUrl;
+        if(imageurl == null || imageurl.trim() == "") return "0";
+        else return imageurl.trim();
     }
 
     public String getDateAdded(){
-        return dateAdded;
+        if(dateadded == null || dateadded.trim() == "") return "1900-01-01 00:00:00";
+        else return dateadded.trim();
     }
 
     public String getLat(){
-        return lat;
+        if(lat == null || lat.trim() == "") return "0";
+        else return lat.trim();
     }
 
     public String getLng(){
-        return lng;
+        if(lng == null || lng.trim() == "") return "0";
+        else return lng.trim();
     }
 }
